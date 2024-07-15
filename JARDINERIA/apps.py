478fb8f3723da6ea06fe303416965231710c2bbf
SebaNeira26 <you@example.com>
2024-07-15 02:00:47 -0400

    errores de tipeo corregidos

--- JARDINERIA/apps.py
+++ JARDINERIA/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class JardineraConfig(AppConfig):
+class JardineriaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'JARDINERA'
+    name = 'JARDINERIA'
+    
